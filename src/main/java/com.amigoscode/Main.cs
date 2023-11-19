055ec14597b913bddfd37b250794d13ea64d4cba
kitou88C39 <kanfutrooper@icloud.com>
2023-11-19 11:52:37 +0900

    com.amigoscode/Main.java 作成

---
+++ src/main/java/com.amigoscode/Main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.main.java.com.amigoscode
+{
+    public class Main
+    {
+        
+    }
+}
